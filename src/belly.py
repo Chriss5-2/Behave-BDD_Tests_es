45f15b8ca8c5a102a12ae2af177b13aca9e94c00
Chriss5-2 <chris.luna.j@uni.pe>
2025-04-17 18:55:36 -0500

    Add script in belly.py

--- src/belly.py
+++ src/belly.py
@@ -1,1 +1,14 @@
-#First line in belly.py
+class Belly:
+    def __init__(self):
+        self.pepinos_comidos = 0
+        self.tiempo_esperado = 0
+
+    def comer(self, pepinos):
+        self.pepinos_comidos += pepinos
+
+    def esperar(self, tiempo_en_horas):
+        self.tiempo_esperado += tiempo_en_horas
+
+    def esta_grunendo(self):
+        # El estómago gruñe si ha esperado al menos 1.5 horas y ha comido más de 10 pepinos
+        return self.tiempo_esperado >= 1.5 and self.pepinos_comidos > 10
