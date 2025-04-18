7f6aba1f25fdf3a148b0d81e5e17e2ebd378cb7a
Chriss5-2 <chris.luna.j@uni.pe>
2025-04-18 03:19:58 -0500

    Update belly.py to accept float

--- src/belly.py
+++ src/belly.py
@@ -1,13 +1,13 @@
 class Belly:
     def __init__(self):
-        self.pepinos_comidos = 0
-        self.tiempo_esperado = 0
+        self.pepinos_comidos = 0.0
+        self.tiempo_esperado = 0.0
 
     def comer(self, pepinos):
-        self.pepinos_comidos += pepinos
+        self.pepinos_comidos += float(pepinos)
 
     def esperar(self, tiempo_en_horas):
-        self.tiempo_esperado += tiempo_en_horas
+        self.tiempo_esperado += float(tiempo_en_horas)
 
     def esta_gruñendo(self):
         # El estómago gruñe si ha esperado al menos 1.5 horas y ha comido más de 10 pepinos
