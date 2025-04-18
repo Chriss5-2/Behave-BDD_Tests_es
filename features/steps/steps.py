b06fed105557c9f0f13b3676b60ff0f236a85fd6
Chriss5-2 <chris.luna.j@uni.pe>
2025-04-18 02:01:35 -0500

    Add more numbers is steps dictionary

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -7,12 +7,14 @@ def convertir_palabra_a_numero(palabra):
         return int(palabra)
     except ValueError:
         numeros = {
-            "cero": 0, "uno": 1, "una": 1, "dos": 2, "tres": 3, "cuatro": 4, "cinco": 5,
+            "quarto":0.25, "cero": 0, "uno": 1, "una": 1, "dos": 2, "tres": 3, "cuatro": 4, "cinco": 5,
             "seis": 6, "siete": 7, "ocho": 8, "nueve": 9, "diez": 10, "once": 11,
             "doce": 12, "trece": 13, "catorce": 14, "quince": 15, "dieciséis": 16,
             "diecisiete": 17, "dieciocho": 18, "diecinueve": 19, "veinte": 20,
             "treinta": 30, "cuarenta": 40, "cincuenta": 50, "sesenta": 60, "setenta": 70,
-            "ochenta": 80, "noventa": 90, "media": 0.5
+            "ochenta": 80, "noventa": 90, "media": 0.5, "cien":100, "cientos":100, "doscientos":200, "trescientos":300, 
+            "cuatrocientos":400, "quinientos":500, "seiscientos":600, "sietecientos":700, "ochocientos":800, 
+            "novecientos":900, "mil":1000
         }
         return numeros.get(palabra.lower(), 0)
 
