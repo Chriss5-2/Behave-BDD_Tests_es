14960b602e50ffc17c5a9e3b865ce2ee9d80c4ba
Chriss5-2 <chris.luna.j@uni.pe>
2025-04-18 02:08:24 -0500

    Solve fix in steps.py

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -12,8 +12,8 @@ def convertir_palabra_a_numero(palabra):
             "doce": 12, "trece": 13, "catorce": 14, "quince": 15, "dieciséis": 16,
             "diecisiete": 17, "dieciocho": 18, "diecinueve": 19, "veinte": 20,
             "treinta": 30, "cuarenta": 40, "cincuenta": 50, "sesenta": 60, "setenta": 70,
-            "ochenta": 80, "noventa": 90, "media": 0.5, "cien":100, "cientos":100, "doscientos":200, "trescientos":300, 
-            "cuatrocientos":400, "quinientos":500, "seiscientos":600, "sietecientos":700, "ochocientos":800, 
+            "ochenta": 80, "noventa": 90, "media": 0.5, "medio":0.5, "cien":100, "cientos":100, "doscientos":200, "trescientos":300, 
+            "cuatrocientos":400, "quinientos":500, "seiscientos":600, "setecientos":700, "ochocientos":800, 
             "novecientos":900, "mil":1000
         }
         return numeros.get(palabra.lower(), 0)
